ae7fc0a0764b42df120e856ad92ce66612afc9a6
shikhars22 <shikhars22@gmail.com>
2022-12-31 17:30:29 +0530

    Lesson 73 done

--- pages/customers/index.tsx
+++ pages/customers/index.tsx
@@ -1,3 +1,53 @@
-export default function Customers() {
-	return <div>Here are the customers: </div>;
-}
+import {
+	GetStaticProps,
+	GetStaticPaths,
+	GetServerSideProps,
+	NextPage,
+	InferGetStaticPropsType,
+} from 'next';
+
+type Customer = {
+	id: number;
+	name: string;
+	industry: string;
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+	return {
+		props: {
+			customers: [
+				{
+					id: 1,
+					name: 'Swiggy',
+					industry: 'Food',
+				},
+				{
+					id: 2,
+					name: 'Reliance',
+					industry: 'Petroleum',
+				},
+			],
+		},
+	};
+};
+
+const Customers: NextPage = ({
+	customers,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+	console.log(customers);
+	return (
+		<>
+			<h1>Here are the customers: </h1>
+			{customers.map((customer: Customer) => {
+				return (
+					<div key={customer.id}>
+						<p>{customer.name}</p>
+						<p>{customer.industry}</p>
+					</div>
+				);
+			})}
+		</>
+	);
+};
+
+export default Customers;
