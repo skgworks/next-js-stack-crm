d3a9690fadc328b5d576de4de1f398cde1ffb334
Nijesh Nj <nijeshnarayanan7@gmail.com>
2023-02-06 16:19:29 +0530

    Auth0 update

--- pages/api/auth/[...auth0].ts
+++ pages/api/auth/[...auth0].ts
@@ -1,7 +1,10 @@
-import { handleAuth, handleLogout } from "@auth0/nextjs-auth0";
+import { handleAuth, handleLogin, handleLogout } from "@auth0/nextjs-auth0";
 
 export default handleAuth({
+  login: handleLogin((req) => {
+    return { returnTo: `https://${process.env.VERCEL_URL}` };
+  }),
   logout: handleLogout((req) => {
-    return { returnTo: `http://${process.env.VERCEL_URL}/logout` };
+    return { returnTo: `https://${process.env.VERCEL_URL}/logout` };
   }),
 });
