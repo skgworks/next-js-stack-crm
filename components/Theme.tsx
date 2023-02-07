0cb0abbc8d2e32d19a9dad9604c416a1a8b07e80
Nijesh Nj <nijeshnarayanan7@gmail.com>
2023-02-07 08:05:29 +0530

    Fixed landing page display issue

--- components/Theme.tsx
+++ components/Theme.tsx
@@ -7,6 +7,9 @@ import Header from "./header";
 import Sidebar from "./sidebar";
 import Content from "./Content";
 import NavigationContextProvider from "./NavigationContextProvider";
+import RenderIf from "./render-if";
+import { useAppSelector } from "../hooks/common/useRedux";
+import { selectLoggedIn } from "../redux/user/selectors";
 
 //You can all the theme properties here. https://mui.com/material-ui/customization/palette/.
 // Just an example is mentioned below.
@@ -17,16 +20,22 @@ const mdTheme = createTheme({
 });
 
 export default function Theme(props: any) {
+  const isLoggedIn = useAppSelector((state) => selectLoggedIn(state));
   return (
-    <ThemeProvider theme={mdTheme}>
-      <Box sx={{ display: "flex" }}>
-        <CssBaseline />
-        <NavigationContextProvider>
-          <Header />
-          <Sidebar />
-        </NavigationContextProvider>
-        <Content props={props} />
-      </Box>
-    </ThemeProvider>
+    <>
+      <RenderIf condition={!!isLoggedIn}>
+        <ThemeProvider theme={mdTheme}>
+          <Box sx={{ display: "flex" }}>
+            <CssBaseline />
+            <NavigationContextProvider>
+              <Header />
+              <Sidebar />
+            </NavigationContextProvider>
+            <Content props={props} />
+          </Box>
+        </ThemeProvider>
+      </RenderIf>
+      <RenderIf condition={!isLoggedIn}>{props.children}</RenderIf>
+    </>
   );
 }
